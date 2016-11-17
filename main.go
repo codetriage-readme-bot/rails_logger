9d1b8047415b0faf74e347dcb5ca596c1cd3ba07
Rajesh Sharma <rajesh@unity3d.com>
2016-11-17 22:43:24 +0100

    Add basic parsing

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/hpcloud/tail"
 	"regexp"
+	"strings"
 )
 
 type DataStore struct {
@@ -12,6 +13,46 @@ type DataStore struct {
 
 type LogItem struct {
 	rawData string
+}
+
+type ParsedLog struct {
+	url                      string
+	method                   string
+	timestamp                string
+	responseCode             string
+	responseTime             string
+	genesisTotalResponseTime string
+}
+
+func (l ParsedLog) headline() string {
+	var headlineItems = make([]string, 3)
+	headlineItems = append(headlineItems, fmt.Sprintf("%s\t%s\t%s", l.responseCode, l.method, l.url))
+	if l.responseTime != "" {
+		headlineItems = append(headlineItems, fmt.Sprintf("\nin %sms", l.responseTime))
+	}
+	if l.genesisTotalResponseTime != "" {
+		headlineItems = append(headlineItems, fmt.Sprintf("api:%sms", l.genesisTotalResponseTime))
+	}
+	return strings.Join(headlineItems, " ")
+}
+
+func (l LogItem) parsedLog() ParsedLog {
+	log := new(ParsedLog)
+	var started_regex = regexp.MustCompile(`Started (\w+) "(.*?)" for ([a-zA-Z:0-9\.]*) at (.*)`)
+	if started_regex.MatchString(l.rawData) {
+		var matches = started_regex.FindStringSubmatch(l.rawData)
+		log.url = matches[2]
+		log.method = matches[1]
+		log.timestamp = matches[4]
+	}
+	var completed_regex = regexp.MustCompile(`Completed\s*(?P<response_code>\w+)\s*(?P<response_description>[\w\s]+) in (?P<response_time>\d+)ms(?:\s\(Genesis: (?P<genesis_total_response_time>\d+\.\d*)ms\))?`)
+	if completed_regex.MatchString(l.rawData) {
+		var matches = completed_regex.FindStringSubmatch(l.rawData)
+		log.responseCode = matches[1]
+		log.responseTime = matches[3]
+		log.genesisTotalResponseTime = matches[4]
+	}
+	return *log
 }
 
 var dataStore DataStore
@@ -51,7 +92,6 @@ func parser() {
 func echoer() {
 	for {
 		log := <-logItems
-		fmt.Println(log.rawData)
-		fmt.Println("##########################\n\n")
+		fmt.Println(log.parsedLog().headline())
 	}
 }
